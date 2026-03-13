ed89c4ba2094be5b7000fd8aa6fad10ad5014488
Rafaela <rafaelasouzasilva3009@gmail.com>
2026-03-13 07:20:59 -0300

    SWAGGGER

--- APIs/api_ordem_servicos/config/swagger.js
+++ APIs/api_ordem_servicos/config/swagger.js
@@ -1,26 +1,31 @@
 //Esse tipo de documento consegue documentar a nossa API
 //Além de servir como uma ferramenta para a gente testar
-//Ele não é nossa API - nós ainda precisamos montar as rotas e tudo  - dependente da API
+//Ele não é nossa API - nós ainda precisamos montar as rotas e tudo
 
 const documentacao = {
-    openapi: `3.0.3`,
+    openapi: '3.0.3',
+
     info: {
-        title: `API Ordens de Serviços`,
-        description: `Documentação da API de Ordens de Serviços`,
-        version: `1.0.0`
-    }, //conseguimos colocar quantos servidores a gente quer aqui 
+        title: 'API Ordens de Serviços',
+        description: 'Documentação da API de Ordens de Serviços',
+        version: '1.0.0'
+    },
+
     servers: [
         {
             url: 'http://localhost:3000',
             description: 'Localhost'
         }
     ],
-    tags: [ //cada separação que temos ele vai para um local especifico 
+
+    tags: [
         { name: "Usuários", description: "Operações relacionadas aos usuários" },
-        {name: "Departamentos", description: "Operações relacionadas a departamentos"},
-        {name: "Ordem de Serviços", description: "Operações relacionadas a ordem de serviços"}
+        { name: "Departamentos", description: "Operações relacionadas a departamentos" },
+        { name: "Ordem_Servicos", description: "Operações relacionadas às ordens de serviço" }
     ],
+
     paths: {
+
         "/usuarios": {
             get: {
                 tags: ["Usuários"],
@@ -29,89 +34,308 @@ const documentacao = {
                     200: {
                         description: "Dados obtidos com sucesso!",
                         content: {
-                            "apllication/json": {
+                            "application/json": {
                                 schema: {
                                     type: "array",
-                                    items: { $ref: "#/components/schemas/Lista_Usuarios" } //aqui é como se fosse uma ancoragem 
-                                }
-                            }
-                        }
-                    },
-                    "/departamentos":{
-                        get: {
-                            tags: ["Departamentos"],
-                            summary: "Lista dos Departamentos",
-                            response: {
-                                200: {
-                                    description: "Dados obtidos com sucesso!",
-                                    content: {
-                                        "apllication/json": {
-                                            schema: {
-                                                type: "array",
-                                                items: { $ref: "#/components/schemas/Lista_Departamentos"}
-                                            }
-                                        }
-                                    }
-                                },
-                                "/ordem_servicos":{
-                                    get: {
-                                        tags: ["Ordem de Serviços"],
-                                        summary: "Ordem de Serviços",
-                                        response: {
-                                            200: {
-                                                description: "Dados obtidos com sucesso!",
-                                                content: {
-                                                    "apllication/json":  {
-                                                        schema: {
-                                                            type: "array",
-                                                            items: { $ref: "#/components/schemas/Ordem_Servicos"}
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                                    items: { $ref: "#/components/schemas/Lista_Usuarios" }
                                 }
                             }
                         }
                     }
                 }
+            },
+
+            post: {
+                tags: ["Usuários"],
+                summary: "Cadastrar novo usuário",
+                description: "Recebe nome, email e senha",
+                requestBody: {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                $ref: "#/components/schemas/Cadastro_Usuario"
+                            }
+                        }
+                    }
+                },
+                responses: {
+                    201: { description: "Usuário cadastrado com sucesso" },
+                    400: { description: "Erro na requisição" },
+                    500: { description: "Erro interno no servidor" }
+                }
+            }
+        },
+
+        "/usuarios/{id_usuario}": {
+            put: {
+                tags: ["Usuários"],
+                summary: "Atualizar usuário completo",
+
+                parameters: [
+                    {
+                        name: "id_usuario",
+                        in: "path",
+                        required: true,
+                        description: "ID do usuário",
+                        schema: { type: "integer" },
+                        example: 1
+                    }
+                ],
+
+                requestBody: {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                $ref: "#/components/schemas/Atualizacao_Usuario"
+                            }
+                        }
+                    }
+                },
+
+                responses: {
+                    200: { description: "Usuário atualizado" },
+                    404: { description: "Usuário não encontrado" },
+                    500: { description: "Erro no servidor" }
+                }
+            }
+        },
+
+        "/departamentos": {
+            get: {
+                tags: ["Departamentos"],
+                summary: "Listar Departamentos",
+
+                responses: {
+                    200: {
+                        description: "Dados obtidos com sucesso!",
+                        content: {
+                            "application/json": {
+                                schema: {
+                                    type: "array",
+                                    items: { $ref: "#/components/schemas/Lista_Departamentos" }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+
+            post: {
+                tags: ["Departamentos"],
+                summary: "Cadastrar novo departamento",
+
+                requestBody: {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                $ref: "#/components/schemas/Cadastro_Departamento"
+                            }
+                        }
+                    }
+                },
+
+                responses: {
+                    201: { description: "Departamento cadastrado" },
+                    400: { description: "Erro na requisição" },
+                    500: { description: "Erro no servidor" }
+                }
+            }
+        },
+
+        "/departamentos/{id_departamento}": {
+            put: {
+                tags: ["Departamentos"],
+                summary: "Atualizar departamento",
+
+                parameters: [
+                    {
+                        name: "id_departamento",
+                        in: "path",
+                        required: true,
+                        schema: { type: "integer" },
+                        example: 1
+                    }
+                ],
+
+                requestBody: {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                $ref: "#/components/schemas/Atualizacao_Departamento"
+                            }
+                        }
+                    }
+                },
+
+                responses: {
+                    200: { description: "Departamento atualizado" },
+                    404: { description: "Departamento não encontrado" },
+                    500: { description: "Erro no servidor" }
+                }
+            }
+        },
+
+        "/ordem_servicos": {
+            get: {
+                tags: ["Ordem_Servicos"],
+                summary: "Listar ordens de serviço",
+                responses: {
+                    200: {
+                        description: "Dados obtidos com sucesso",
+                        content: {
+                            "application/json": {
+                                schema: {
+                                    type: "array",
+                                    items: { $ref: "#/components/schemas/Lista_Ordem_Servicos" }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+
+            post: {
+                tags: ["Ordem_Servicos"],
+                summary: "Cadastrar nova ordem de serviço",
+
+                requestBody: {
+                    required: true,
+                    content: {
+                        "application/json": {
+                            schema: {
+                                $ref: "#/components/schemas/Cadastro_Ordem_Servicos"
+                            }
+                        }
+                    }
+                },
+
+                responses: {
+                    201: { description: "Ordem cadastrada com sucesso" },
+                    400: { description: "Erro na requisição" },
+                    500: { description: "Erro no servidor" }
+                }
+            }
+        },
+
+        "/ordem_servicos/{id_ordem}": {
+            put: {
+                tags: ["Ordem_Servicos"],
+                summary: "Atualizar ordem de serviço",
+
+                parameters: [
+                    {
+                        name: "id_ordem",
+                        in: "path",
+                        required: true,
+                        schema: { type: "integer" },
+                        example: 1
+                    }
+                ],
+
+                responses: {
+                    200: { description: "Ordem atualizada" },
+                    404: { description: "Ordem não encontrada" },
+                    500: { description: "Erro no servidor" }
+                }
             }
         }
     },
+
     components: {
+
         schemas: {
+
             Lista_Usuarios: {
                 type: "object",
                 properties: {
                     id: { type: "integer", example: 1 },
-                    nome: {type: "string", example: "Ricardo"},
-                    email: {type: "string", example: "ricardo@email.com"}
+                    nome: { type: "string", example: "Ricardo" },
+                    email: { type: "string", example: "ricardo@email.com" }
                 }
             },
-            Lista_Departamento: {
+
+            Cadastro_Usuario: {
                 type: "object",
                 properties: {
-                    id: { type: "interger", example: 1 },
-                    nome: { type: "string", example: "sala 02" },
-                    descricao: { type: "string", example: "perto da porta de saida dos fundos do outro bloco" }
+                    nome: { type: "string", example: "Ricardo" },
+                    email: { type: "string", example: "ricardo@email.com" },
+                    senha: { type: "string", example: "senha123" }
                 }
             },
-            Ordem_Servicos: {
+
+            Atualizacao_Usuario: {
+                type: "object",
+                required: ["nome", "email", "senha"],
+                properties: {
+                    nome: { type: "string" },
+                    email: { type: "string" },
+                    senha: { type: "string" }
+                }
+            },
+
+            Lista_Departamentos: {
                 type: "object",
                 properties: {
-                    id: {  type: "interger", example: 1 },
-                    numero_ordem: { type: "interger", example: 1 },
-                    titulo: { type: "string", example: "cadeira" },
-                    descricao: { type: "string", example: "sem a perna" },
-                    prioridade: { type: "string", example: "alta" },
-                    status: { type: "string", example: "em andamento" },
+                    id: { type: "integer", example: 1 },
+                    nome: { type: "string", example: "Manutenção" },
+                    descricao: { type: "string", example: "Setor de manutenção geral" }
+                }
+            },
+
+            Cadastro_Departamento: {
+                type: "object",
+                properties: {
+                    nome: { type: "string", example: "Manutenção" },
+                    descricao: { type: "string", example: "Setor responsável por reparos" }
+                }
+            },
+
+            Atualizacao_Departamento: {
+                type: "object",
+                required: ["nome", "descricao"],
+                properties: {
+                    nome: { type: "string", example: "CASA" },
+                    descricao: { type: "string",example: "Setor responsável por reparos"  }
+                }
+            },
+
+            Lista_Ordem_Servicos: {
+                type: "object",
+                properties: {
+                    id: { type: "integer", example: 1 },
+                    titulo: { type: "string", example: "Troca de lâmpada" },
+                    descricao: { type: "string", example: "Trocar lâmpada da sala 3" },
+                    prioridade: { type: "string", example: "Alta" },
+                    status: { type: "string", example: "Aberta" }
+                }
+            },
+
+            Cadastro_Ordem_Servicos: {
+                type: "object",
+                properties: {
+                    titulo: { type: "string", example: "Troca de lâmpada" },
+                    descricao: { type: "string", example: "Trocar lâmpada da sala 3" },
+                    prioridade: { type: "string", example: "Alta" },
+                    status: { type: "string", example: "Aberta" }
+                }
+            },
+            Atualizacao_Ordem_Servicos: {
+                type: "object",
+                required: ["titulo", "descricao", "prioridade", "status"],
+                properties: {
+                    titulo: { type: "string", example: "CASA DE SERVIÇOS" },
+                    descricao: { type: "string",example: "Setor responsável por reparos"  },
+                    prioridade: { type: "string",example: "Alta"  },
+                    status: { type: "string",example: "Aberta"  },
                 }
             }
+
         }
     }
-
 }
-
 
 export default documentacao
--- APIs/api_ordem_servicos/src/routes/rotasUsuarios.js
+++ APIs/api_ordem_servicos/src/routes/rotasUsuarios.js
@@ -61,14 +61,135 @@ router.get('/ordem_servicos', async(req, res) =>{
         const ordem_servicos = await BD.query(query);   //executando dentro do banco de dados a query armazenada chamando sua function
         
         //retorno para a pagina o json com os dados buscados do SQL
-        res.status(200).json(ordem_servicos.rows); //rows são todas as linhas que foram retornadas da nossa query status 200ok
+        return res.status(200).json(ordem_servicos.rows); //rows são todas as linhas que foram retornadas da nossa query status 200ok
 
     }catch(error){
         console.log('Erro ao listar ordem_servicos', error.message);
-        res.status(500).json({error: 'Erro ao listar ordem_servicos'})
+        return res.status(500).json({error: 'Erro ao listar ordem_servicos'})
         
     }
 })
 
+// CRIAÇÃO DE UMA ROTA (ENDPOINT) QUE EXISTE A POSSIBILIDADE DE UMA INVASÃO -----------------------------------------------------------------------
+   //ENDPOINT COM PARAMETROS DIRETOS NO COMANDO SQL PERMITE O SQL INJECTION
+router.post('/usuarios', async(req, res) => {
+    const { nome, email, senha } = req.body;
+    try{                                                  //o VALUES não é colocado os valores da const pois se não ficara aberto a hacker
+    const comando = `INSERT INTO USUARIOS(nome, email, senha) VALUES($1, $2, $3)`   //criação de uma variavel para armazenar nosso comando SQL 
+    const valores = [nome, email, senha];
+
+    await BD.query(comando, valores)
+    console.log(comando, valores);
+
+    return res.status(201).json("Usuario cadastrado.");
+    }catch(error){
+        console.log('Erro ao cadastrar usuarios', error.message);
+        return res.status(500).json({error: 'Erro ao cadastrar usuarios'})
+        
+    }
+})
+
+//usando nosso metodo put de atualizar e editar
+    //CRIAÇÃO DO ENDPOINT PARA ATUALIZAR UM UNICO USUARIO
+            //recebendo o parametro pelo ID e buscando o usuario
+router.put('/usuarios/:id_usuario', async(req, res) => {
+    //id recebido via parametro
+    const {id_usuario} = req.params;
+    //dados do usuario recebido via corpo da pagina
+    const {nome, email, senha} = req.body;
+    try{
+        //verificar se o usuario existe 
+        const verificarUsuario = await BD.query(`SELECT * FROM USUARIOS WHERE id_usuario = $1`, [id_usuario])
+        if(verificarUsuario.rows.length === 0){
+            return res.status(404).json({message: 'Usuario não encontrado'})
+        }
+        //atualiza todos os campos da tabela(PUT SUBSTITUIÇÃO COMPLETA)
+                                            //evitando que todos os usuarios sejam alterados
+        const comando = `UPDATE USUARIOS SET nome = $1, email = $2, senha = $3 WHERE id_usuario = $4`;
+        const valores = [nome, email, senha, id_usuario];
+        await BD.query(comando, valores);
+        return res.status(200).json('Usuario foi atualizado!')
+        }catch(error){
+        console.log('Erro ao cadastrar usuarios', error.message);
+        return res.status(500).json({error: 'Erro ao atualizar usuarios'})
+    }
+})
+
+//usando nosso metodo put de atualizar e editar
+    //CRIAÇÃO DO ENDPOINT PARA ATUALIZAR UM UNICO DEPARTAMENTO
+            //recebendo o parametro pelo ID e buscando o usuario
+router.put('/departamentos/:id_departamento', async(req, res) => {
+    //id recebido via parametro
+    const {id_departamento} = req.params;
+    //dados do usuario recebido via corpo da pagina
+    const {nome, descricao} = req.body;
+    try{
+        //verificar se o usuario existe 
+        const verificarDepartamento = await BD.query(`SELECT * FROM DEPARTAMENTOS WHERE id_departamento = $1`, [id_departamento])
+        if(verificarDepartamento.rows.length === 0){
+            return res.status(404).json({message: 'Departamento não encontrado'})
+        }
+        //atualiza todos os campos da tabela(PUT SUBSTITUIÇÃO COMPLETA)
+                                            //evitando que todos os usuarios sejam alterados
+        const comando = `UPDATE DEPARTAMENTOS SET nome = $1, descricao = $2 WHERE id_departamento = $4`;
+        const valores = [nome, descricao, id_departamento];
+        await BD.query(comando, valores);
+        return res.status(200).json('Departamento foi atualizado!')
+        }catch(error){
+        console.log('Erro ao cadastrar departamentos', error.message);
+        return res.status(500).json({error: 'Erro ao atualizar departamentos'})
+    }
+})
+
+//usando nosso metodo put de atualizar e editar
+    //CRIAÇÃO DO ENDPOINT PARA ATUALIZAR UM UNICO ORDEM_SERVICOS
+            //recebendo o parametro pelo ID e buscando o usuario
+router.put('/ordem_servicos/:id_ordem', async(req, res) => {
+    //id recebido via parametro
+    const {id_ordem} = req.params;
+    //dados do usuario recebido via corpo da pagina
+    const {numero_ordem, titulo, descricao, prioridade, status, data} = req.body;
+    try{
+        //verificar se o usuario existe 
+        const verificarOrdemServicos = await BD.query(`SELECT * FROM ORDEM_SERVICOS WHERE id_ordem = $1`, [id_ordem])
+        if(verificarOrdemServicos.rows.length === 0){
+            return res.status(404).json({message: 'Ordem de serviços não encontrado'})
+        }
+        //atualiza todos os campos da tabela(PUT SUBSTITUIÇÃO COMPLETA)
+                                            //evitando que todos os usuarios sejam alterados
+        const comando = `UPDATE DEPARTAMENTOS SET numero_ordem = $1, titulo = $2, descricao = $3, prioridade = $4, status = $5, data = $6 WHERE id_departamento = $7`;
+        const valores = [numero_ordem, titulo, descricao, prioridade, status, data, id_ordem];
+        await BD.query(comando, valores);
+        return res.status(200).json('Ordem de serviços foi atualizado!')
+        }catch(error){
+        console.log('Erro ao cadastrar ordens', error.message);
+        return res.status(500).json({error: 'Erro ao atualizar ordens'})
+    }
+})
+
+
 
 export default router
+
+
+
+
+// // CRIAÇÃO DE UMA ROTA (ENDPOINT) QUE EXISTE A POSSIBILIDADE DE UMA INVASÃO -----------------------------------------------------------------------
+//    //ENDPOINT COM PARAMETROS DIRETOS NO COMANDO SQL PERMITE O SQL INJECTION
+// router.post('/usuarios', async(req, res) => {
+//     const nome = req.body.nome;
+//     const email = req.body.email;
+//     const senha = req.body.senha;
+//     try{
+//     const comando = `INSERT INTO teste(nome, email, senha) 
+//     VALUES('${nome}', '${email}', '${senha}')`   //criação de uma variavel para armazenar nosso comando SQL 
+//     console.log(comando);
+//     await BD.query(comando)
+//     res.status(201).json("Usuario cadastrado.");
+//     }catch(error){
+//         console.log('Erro ao cadastrar usuarios', error.message);
+//         res.status(500).json({error: 'Erro ao cadastrar usuarios'})
+        
+//     }
+// })
+//----------------------------------------------------------------------------------------------------------------------------------------------
